fff759eabd0e2400b85b4039031049e83dc48d73
Francis <fbell884@gmail.com>
2022-09-28 16:43:01 -0400

    env variables netlify

--- weather.js
+++ weather.js
@@ -25,12 +25,15 @@ var timeMobile = document.getElementById("time-mobile");
 timePara.innerHTML = hourMin;
 timeMobile.innerHTML = hourMin;
 
+const APIKey = process.env.WEATHER_API_KEY;
+console.log(APIKey);
+
 // Get Weather Data
 function getData() {
 
     var zipCode = document.getElementById('zipcode').value;
-    var APIKey = process.env.WEATHER_API_KEY;
-    console.log(APIKey);
+    // const APIKey = process.env.WEATHER_API_KEY;
+    // console.log(APIKey);
 
     var url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${APIKey}&units=imperial`;
 
