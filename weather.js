a5f6a9b6098520b16da88b25f52b120c99cdb19e
Francis <fbell884@gmail.com>
2022-12-15 14:43:35 -0500

    update

--- weather.js
+++ weather.js
@@ -10,7 +10,6 @@ var sunriseEl = document.getElementById("riseTime");
 var sunsetEl = document.getElementById("sunsetTime");
 var windSpdEl = document.getElementById("windSpeed");
 var humidityEl = document.getElementById("humidPercent");
-const apiKey = process.env.WEATHER_API_KEY;
 
 // get current time 
 var date = new Date();
@@ -30,7 +29,7 @@ timeMobile.innerHTML = hourMin;
 function getData() {
 
     var zipCode = document.getElementById('zipcode').value;
-    var url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=imperial`;
+    var url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=4c4bf9966171c40dc8fc61454992bf5f&units=imperial`;
 
     // fetch weather data
     fetch(url, {
